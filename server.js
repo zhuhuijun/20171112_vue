de0e3c91634dd987335de6442487fce42ecd28be
zhuhj <zhuhjtcx@qq.com>
2017-11-16 08:43:15 +0800

    45

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://101.201,43.141/leaves', {useMongoClient: true});
+var leaveSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    createAt: {type: Date, default: Date.now}
+});
+mongoose.model('Leave', leaveSchema);
+var app = express();
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.listen(3000);
+app.get('/', function (req, res) {
+    res.sendFile('./8.leave.html', {root: __dirname});
+});
+app.post('/leaves', function (req, res) {
+    var data = req.body;
+    console.info(data);
+    mongoose.model('Leave').create(data, function (err,doc) {
+        console.info(err);
+        res.send(doc);
+    });
+    res.send(data);
+})
